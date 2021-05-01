75effccc905468bb8ea82a8041301a2458dddbda
rubbyhorn <80526646+rubbyhorn@users.noreply.github.com>
2021-05-01 20:26:23 +0300

    parce items to file

--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
 import json
 import requests
 import traceback
-from urllib.parse import urlparse, urlunparse
+from urllib.parse import urlparse, urljoin
+from bs4 import BeautifulSoup
+import bs4.element
 
 DEBUG = True
 
@@ -44,6 +46,72 @@ def get_page(url: str, query: str) -> str:
     return response.text
 
 
+class Entity:
+    def __init__(self, domain: str, tag: bs4.element.Tag):
+        try:
+            self.id = tag.get("id")
+        except:
+            self.id = None
+
+        try:
+            self.url = urljoin(domain, tag.select('a[itemprop="url"]').pop().get("href"))
+        except:
+            self.url = None
+
+        try:
+            self.imgUrl = tag.select('a[itemprop="url"] li')\
+                .pop().get("data-marker").replace("slider-image/image-", "")
+        except:
+            self.imgUrl = None
+
+        try:
+            self.title = tag.select('a[itemprop="url"]')\
+                .pop().get("title")
+        except:
+            self.title = None
+
+        try:
+            self.price = tag.select('span[data-marker="item-price"] meta[itemprop="price"]')\
+                .pop().get("content")
+        except:
+            self.price = None
+
+        try:
+            self.priceCurrency = tag.select('span[data-marker="item-price"] meta[itemprop="priceCurrency"]')\
+                .pop().get("content")
+        except:
+            self.priceCurrency = None
+
+        try:
+            self.lifeTime = tag.select('div[data-marker="item-date"]')\
+                .pop().text
+        except:
+            self.lifeTime = None
+
+        try:
+            self.geo = tag.select('span[class^="geo"]')\
+                .pop().text
+        except:
+            self.geo = None
+
+    def __str__(self):
+        return "id: {id}\n" \
+               "url: {url}\n" \
+               "imgUrl: {imgUrl}\n" \
+               "title: {title}\n" \
+               "price: {price}\n" \
+               "lifeTime: {lifeTime}\n" \
+               "geo: {geo}\n" \
+               "\n".\
+                format(id=self.id,
+                       url=self.url,
+                       imgUrl=self.imgUrl,
+                       title=self.title,
+                       price=self.price+" "+self.priceCurrency,
+                       lifeTime=self.lifeTime,
+                       geo=self.geo)
+
+
 def main():
     sites = parse_config()
     try:
@@ -51,16 +119,22 @@ def main():
             for query in site["queries"]:
                 try:
                     url = site["url"]
+                    domain = '{uri.scheme}://{uri.netloc}/'.format(uri=urlparse(url))
                     try:
                         page = get_page(url, query)
                     except requests.exceptions.RequestException as e:
                         print("Ашибка HTTP. Чет с сетью.\n{}".format(e))
                         raise e
+                    soup = BeautifulSoup(page, "lxml")
+
+                    results = map(lambda x: Entity(domain, x),
+                                  soup.select('div[data-marker="catalog-serp"] div[data-marker="item"]'))
                     filename = "_".join((urlparse(url).netloc.replace(".", "_"),
                                          query.replace(" ", "_"))) + '.html'
 
                     with open(filename, "wb")as file:
-                        file.write(bytes(page, encoding='utf-8'))
+                        for result in results:
+                            file.write(bytes(str(result), encoding='utf-8'))
                 except BaseException as e:
                     print("Ошибка при загрузке.")
                     if DEBUG:
